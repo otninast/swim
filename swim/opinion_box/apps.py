abde4497b769a04e59caa462e52a9e7f0ddea092
tomoya <otninast38@gmail.com>
2018-06-06 08:01:15 +0900

    ignore accounts

--- swim/opinion_box/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpinionBoxConfig(AppConfig):
-    name = 'opinion_box'
--- swim/opinion_box/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post_Opinion(models.Model):
-    content = models.CharField(max_length=140, verbose_name='本文')
